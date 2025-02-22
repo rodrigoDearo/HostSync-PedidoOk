d35b44de8780beda47162623cb7e61c03fb5cde0
rodrigoDearo <rodrigonascimentodearo@outlook.com>
2025-02-22 19:13:06 -0300

    .

--- src/main/utils/preparingRequest.js
+++ src/main/utils/preparingRequest.js
@@ -2,10 +2,23 @@ const { postProduct, putProduct, deleteProduct, undeleteProduct, postCustomer, p
 const { returnValueFromJson } = require('./manageInfoUser');
 const { returnInfo } = require('../envManager');
 
-async function preparingPostProduct(){
+async function preparingPostProduct(product){
     return new Promise(async (resolve, reject) => {
+        let body, header
 
-    })
+        await returnHeader()
+        .then(async (response) => {
+            header = response;
+            let i  = await returnBody(product);
+            return i
+        })
+        .then(async (response) => {
+            body = response
+            await postProduct(body, header)
+        }) 
+
+        
+    })  
 }
 
 
@@ -60,6 +73,33 @@ async function preparingUndeleteCustomer(){
 
 async function returnHeader(){
     return new Promise(async (resolve, reject) => {
-        
+        let tokenParceiro, tokenPedidoOk;
+
+        await returnInfo('token_partner')
+        .then(response => {
+            tokenParceiro = response
+        })
+
+        await returnValueFromJson('tokenpedidook')
+        .then(response => {
+            tokenPedidoOk = response
+        })
+
+        const header = {
+            'token_parceiro': `${tokenParceiro}}`,
+            'token_pedidooK': `${tokenPedidoOk}`,
+            'Content-Type': 'application/json'
+        }
+
+        resolve(header)
+    })
+}
+
+
+async function returnBody(productHost){
+    return new Promise(async (resolve, reject) => {
+        let productToPediodoOk = {
+
+        }
     })
 }
