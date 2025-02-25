89efd1dac571d64858eeff4d3d18216350752a53
rodrigoDearo <rodrigonascimentodearo@outlook.com>
2025-02-25 20:44:13 -0300

    add an handle on success requests

--- src/main/utils/auxFunctions.js
+++ src/main/utils/auxFunctions.js
@@ -12,8 +12,7 @@ async function returnConfigToAccessDB(){
                 host: 'localhost',
                 port: 3050,
                 database: `${response}/HOST.FDB`,
-                charset: 'UTF8',
-                encoding: 'UTF8'
+                charset: 'UTF8'
             }
         })
         .then(async () => {
@@ -55,25 +54,89 @@ function gravarLog(mensagem) {
 async function incrementIdRequestPost(){
   return new Promise(async (resolve, reject) => {
     const configApp = JSON.parse(fs.readFileSync('./config/configApp.json', 'utf-8'));
-    configApp.pedidoOk.idparceiro++;
-    fs.writeFileSync('./config/configApp.json', JSON.stringify(configApp))
-
+    configApp.pedidoOk.idRequestPost++;
+    fs.writeFileSync('./config/configApp.json', JSON.stringify(configApp), 'utf-8')
     resolve()
   })
 }
 
 
-async function errorHandling(){
+async function errorHandlingRequest(){
 
 }
 
 
-async function succesHandling(){
+async function succesHandlingRequests(destiny, resource, idHost, idPedOk){
+  return new Promise(async (resolve, reject) => {
 
+    if(destiny=="product"){
+      let productsDB = JSON.parse(fs.readFileSync('../../../config/products.json'))
+
+      switch (resource) {
+        case "post":
+          productsDB[`${idHost}`] = {
+            "idPedidoOk": `${idPedOk}`,
+            "status": "ATIVO"
+          }
+          await incrementIdRequestPost();
+          break;
+
+        case "update":
+          
+          break;
+
+        case "delete":
+          productsDB[`${idHost}`].status = "INATIVO";
+          break;
+
+        case "undelete":
+          productsDB[`${idHost}`].status = "ATIVO";
+          break;
+      }
+
+      fs.writeFileSync('../../../config/products.json', JSON.stringify(productsDB), 'utf-8')
+
+    }else
+    if(destiny=="customer"){
+      let customersDB = JSON.parse(fs.readFileSync('../../../config/customers.json'))
+
+      switch (resource) {
+        case "post":
+          customersDB[`${idHost}`] = {
+            "idPedidoOk": `${idPedOk}`,
+            "status": "ATIVO"
+          }
+          await incrementIdRequestPost();
+          break;
+
+          case "update":
+          
+          break;
+
+        case "delete":
+          customersDB[`${idHost}`].status = "INATIVO";
+          break;
+
+        case "undelete":
+          customersDB[`${idHost}`].status = "ATIVO";
+          break;
+      }
+
+      fs.writeFileSync('../../../config/customers.json', JSON.stringify(customersDB), 'utf-8')
+      
+    }else
+    if(destiny=="sales"){
+      let configDB = JSON.parse(fs.readFileSync('../../../config/configApp.json'))
+      
+
+      fs.writeFileSync('../../../config/configApp.json', JSON.stringify(configDB), 'utf-8')
+    }
+
+  })
 }
 
 module.exports = {
     returnConfigToAccessDB,
-    incrementIdParceiro,
+    incrementIdRequestPost,
     gravarLog
 }
